c8b64ff96a2759e2cae5a1e724ce552dcc84695f
Carolina Moutella <ola@carolinamoutella.com>
2021-06-17 18:12:09 -0300

    Hide/expand subtasks

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.css';
 
 const Task = ({ task, tasks, setTasks }) => {
+  const [expanded, setExpanded] = useState(false);
+
   const handleDone = () => {
     handleStatus('done');
   }
@@ -32,10 +34,16 @@ const Task = ({ task, tasks, setTasks }) => {
     setTasks(tasks.filter((t) => t.id !== task.id));
   }
 
+  const handleExpand = () => {
+    setExpanded(!expanded);
+  }
+
   return(
-    <li className={`task ${task.status}`}>
+    <li 
+      className={`task ${task.status} ${expanded ? 'expanded' : ''}`}
+      onClick={handleExpand}>
       <div className="task-header df df-a-c df-j-sb">
-        <p className="task-title">{task.title}</p>
+        <p className="task-title df df-a-c"><span></span> {task.title}</p>
         <div className="task-status df">
           <button 
             className={`task-status-tag`}
@@ -50,7 +58,7 @@ const Task = ({ task, tasks, setTasks }) => {
           </div>
         </div>
       </div>
-      <div className="task-body">
+      <div className={`task-body ${expanded ? '' : 'hidden'}`}>
         <ul className="task-subtasks">
           {task.subtasks.map((item) => (
             <li className={`task-subtasks-item df df-a-c ${item.status}`} key={item.id}>
